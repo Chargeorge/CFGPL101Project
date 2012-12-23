9a8cc9701b206e1d0cdc06483379d3e578a6ac51
Charles George <Chargeorge@gmail.com>
2012-12-23 18:53:04 -0500

    New Mus!

--- Mus/mustest.js
+++ Mus/mustest.js
@@ -1,38 +1,76 @@
-var compileInner = function(musexpr, notearray, timetonow){
-    if(musexpr.tag == 'seq')
-    {
-        timetonow = compileInner(musexpr.left,notearray, timetonow);
-        timetonow = compileInner(musexpr.right,notearray, timetonow);
-        return timetonow;
-    } 
-    else
-    {
-        notearray[notearray.length] = {
-            tag : 'note', 
-            pitch: musexpr.pitch, 
-            dur: musexpr.dur,
-            start: timetonow
-        };
-        return timetonow + musexpr.dur;
+// maybe some helper functions
+var endTime = function (time, expr) {
+    // your code here
+    if (expr.tag == 'seq') {
+        return endTime(endTime(time, expr.left), expr.right);
     }
+    else if (expr.tag == 'par') {
+        var left = endTime(time, expr.left);
+        var right = endTime(time, expr.right);
+
+        return (left > right) ? left : right;
+    }
+
+    else {
+
+        return time + expr.dur;
+    }
+
 };
+
+var compileT = function (t, expr, arrayHolder) {
+    if (expr.tag == 'seq') {
+        compileT(t, expr.left, arrayHolder);
+        compileT(endTime(t, expr.left), expr.right, arrayHolder);
+
+    }
+    else if (expr.tag == 'par') {
+        compileT(t, expr.left, arrayHolder);
+        compileT(t, expr.right, arrayHolder);
+
+    }
+    else if (expr.tag == 'rest') {
+        { arrayHolder[arrayHolder.length] = { tag: 'rest',  start: t, dur: expr.dur }; }
+    }
+    else {
+        arrayHolder[arrayHolder.length] = { tag: 'note', pitch: expr.pitch, start: t, dur: expr.dur };
+    }
+
+};
+
+
 
 var compile = function (musexpr) {
-   var returnable = [];
-   compileInner(musexpr, returnable, 0);
-   return returnable;
+    var arrayHolder = [];
+    compileT(0, musexpr, arrayHolder);
+    for (var i = 0; i < arrayHolder.length; i++) {
+        var val = arrayHolder[i];
+    }
+    return arrayHolder;
+
 };
 
-var melody_mus = 
+
+
+
+
+var melody_mus =
     { tag: 'seq',
-      left: 
+        left:
        { tag: 'seq',
-         left: { tag: 'note', pitch: 'a4', dur: 250 },
-         right: { tag: 'note', pitch: 'b4', dur: 250 } },
-      right:
+           left: { tag: 'note', pitch: 'a4', dur: 250 },
+           right: { tag: 'note', pitch: 'b4', dur: 250}
+       },
+        right:
        { tag: 'seq',
-         left: { tag: 'note', pitch: 'c4', dur: 500 },
-         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+           left: { tag: 'note', pitch: 'c4', dur: 500 },
+           right: { tag: 'seq',
+               left: { tag: 'rest', dur: 100 },
+               right: { tag: 'note', pitch: 'd4', dur: 500 }
+           }
+       }
+    };
+
 console.log ("hello world!");
 console.log(melody_mus);
 console.log(compile(melody_mus));
